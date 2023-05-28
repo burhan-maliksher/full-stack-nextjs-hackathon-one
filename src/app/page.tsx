7b21261814ffc64da366c4546af54f7e173ff826
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-05-28 12:30:18 +0500

    card from shadcn ui integrarted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import { CardDemo } from './card'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>hi</div>
+      <div>
+        <CardDemo/>
+      </div>
     </main>
   )
 }
