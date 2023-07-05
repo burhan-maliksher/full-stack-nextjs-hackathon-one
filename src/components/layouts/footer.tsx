d40ecf4578fdd9cf2e3833853ad4a8e092ba4811
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-07-05 08:32:48 +0500

    padding for fotter updated

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3  pl-4 pb-2  border-t border-slate-600 w-full '>
+    <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 place-items-start md:place-items-center pl-8  border-t border-slate-600 w-full '>
       <p className='pt-4'>Copyright © 2022 Dine Market</p>
       <p className='pt-4'>Design by. 
         <span className='font-bold'>Weird Design Studio </span>
