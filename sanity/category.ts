529f9215c5a21eca12a3fcd43421a9487fbb6a46
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-07-14 10:53:29 +0500

    typescript/intelisence added for schema

--- sanity/category.ts
+++ sanity/category.ts
@@ -1,12 +1,14 @@
-export default {
+import { defineType, defineField } from "sanity";
+
+export default defineType({
   name: "category",
   type: "document",
   title: "Category",
   fields: [
-    {
+    defineField({
       name: "name",
       title: "Category Name",
       type: "string",
-    },
+    }),
   ],
-};
+});
--- sanity/product.ts
+++ sanity/product.ts
@@ -1,50 +1,52 @@
-export default {
+import { defineType, defineField } from "sanity";
+
+export default defineType({
   name: "product",
   type: "document",
   title: "Product",
   fields: [
-    {
+    defineField({
       name: "name",
       type: "string",
       title: "Name",
-    },
-    {
+    }),
+    defineField({
       name: "producttype",
       type: "string",
       title: "Product Type",
-    },
-    {
+    }),
+    defineField({
       name: "productdetail",
       type: "string",
       title: "Product Detail",
-    },
-    {
+    }),
+    defineField({
       name: "productcare",
       title: "Product Care",
       type: "array",
       of: [{ type: "string" }],
-    },
-    {
+    }),
+    defineField({
       name: "size",
       title: "size",
       type: "array",
       of: [{ type: "string" }],
-    },
-    {
+    }),
+    defineField({
       name: "quantity",
       type: "number",
       title: "Quanity",
-    },
-    {
+    }),
+    defineField({
       name: "price",
       type: "number",
       title: "Price",
-    },
-    {
+    }),
+    defineField({
       name: "image",
       title: "Image",
       type: "array",
       of: [{ type: "image" }],
-    },
+    }),
   ],
-};
+});
