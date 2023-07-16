17974806649dbac11ece6a087c0d214000ca132e
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-07-16 07:36:48 +0500

    webpack typeerror fixed,window object fuction remaned to pascal to aviod fast redreh issue

--- src/view/layouts/nav.tsx
+++ src/view/layouts/nav.tsx
@@ -17,43 +17,43 @@ export default function Nav() {
 
   const handleResize = () => {
     setIsLargeScreen(window.innerWidth >= 1024);
-    
+
   };
 
-  const windowResize=()=>{
+  const WindowResize = () => {
     if (typeof window !== 'undefined') {
       window.addEventListener('resize', handleResize);
     }
-    
+
     // window.addEventListener('resize', handleResize);
 
   }
 
 
   useEffect(() => {
-    
+
     handleResize();
-    
-  }, [windowResize()]);
+
+  }, [WindowResize()]);
 
   return (
     <div className="mt-2 " >
       <NavigationMenu>
         <NavigationMenuList className="flex justify-between px-6 lg:px-0 lg:pl-6     text-lg w-screen ">
-        {/* logo */}
-        <Link href="/" className="">
-          <Logo/>
-        </Link>
-        <div className="lg:pt-4 lg:px-8">
-          {isLargeScreen ? (
-            <WindowNavBtnList/>
-          ) : (
-            <MenuBtn/>
-          )}
-        </div>
-        
-      </NavigationMenuList>
-    </NavigationMenu>
+          {/* logo */}
+          <Link href="/" className="">
+            <Logo />
+          </Link>
+          <div className="lg:pt-4 lg:px-8">
+            {isLargeScreen ? (
+              <WindowNavBtnList />
+            ) : (
+              <MenuBtn />
+            )}
+          </div>
+
+        </NavigationMenuList>
+      </NavigationMenu>
 
     </div>
   )
@@ -62,21 +62,21 @@ export default function Nav() {
 function WindowNavBtnList() {
   return (
     <div className="flex  xl:w-[75vw] lg:justify-between   lg:space-x-4 lg:text-sm xl:text-lg">
-        <div className="flex xl:pl-8 xl:w-[52vw] xl:justify-between">
-          {/* links */}
-          <NavBtnList/>
-            
-          {/*search bar  */}
-          <div className="lg:ml-4 xl:ml-0 xl:pr-8">
-           <Searchbar/>
+      <div className="flex xl:pl-8 xl:w-[52vw] xl:justify-between">
+        {/* links */}
+        <NavBtnList />
 
-          </div>
-          
-        </div>        
-        {/* cart icon */}
-        <Link href="/cartItems" className=" " >
-          <Cart/>
-        </Link>
+        {/*search bar  */}
+        <div className="lg:ml-4 xl:ml-0 xl:pr-8">
+          <Searchbar />
+
+        </div>
+
+      </div>
+      {/* cart icon */}
+      <Link href="/cartItems" className=" " >
+        <Cart />
+      </Link>
     </div>
   )
 }
