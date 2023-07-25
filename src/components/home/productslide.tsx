a5995d6c49720c9bcd0d5ded32ae733be28ce0db
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-07-25 10:32:45 +0500

    successfully added product slides

--- src/components/home/productslide.tsx
+++ src/components/home/productslide.tsx
@@ -1,8 +1,9 @@
 "use client"
+
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React from "react";
-import Slider from "react-slick";
+import React, { useEffect, useState } from "react";
 import { client } from '@/lib/sanityClient'
 import Image from 'next/image';
 import { IProduct } from '@/types/types';
@@ -21,40 +22,22 @@ const getProductDataFromSanity = async (): Promise<IProduct[]> => {
 
 };
 
-async function AllProducts() {
-  const data = await getProductDataFromSanity()
-  
-  return (
-    <>
-    {
-      data.length!==0?data.map((item) => (
-          <>
-            <div className='flex flex-col justify-center hover:scale-105 transition delay-75'>
-              <Link href={`/products/${ReplaceSpaceWithDash(item.name)}`} >                      
-                <Image key={item.id} src={urlForImage(item.image).width(200).url()} alt='product' width={300} height={300} className='w-full h-auto' />
-              </Link>
-              <h1 key={item.id} className='font-bold'>{item.name}</h1>
-              <h2 key={item.id}>{item.producttype}</h2>
-              <h2 key={item.id} className='font-bold text-lg'>${item.price}</h2>
-            </div>
-          </>
-        )):<div className=' hidden '></div>
-            
-        
+export default  function SimpleSlider():JSX.Element {
+  // using state to convert a promise to a valid jsx element
+  const [allproduct,setAllProduct]=useState<IProduct[]|null>(null)
+
+  useEffect(()=>{
+    async function AllProducts():Promise<void>{
+    const data = await getProductDataFromSanity()
+    setAllProduct(data)
     }
-  {
-    data.length===0?<div className='flex w-full  h-full m-36 pb-24 align-middle justify-center '>
-      <h1 className='text-3xl md:text-4xl lg:text-5xl'>No Item Found</h1>
-    </div>:<div className='hidden'></div>
-    
-  }   
-</>         
-  )
-}
 
+    AllProducts()
 
-export default  function SimpleSlider() {
-    const settings = {
+  },[])
+
+  // for styling for react slick 
+  const settings = {
         dots: false,
         infinite: false,
         speed: 500,
@@ -82,10 +65,22 @@ export default  function SimpleSlider() {
         ]
       };
       
-      return (
-        <Slider {...settings} >
-            {/* <AllProducts/> */}
+      return (<div className="mx-8">
+        <Slider {...settings} className="flex ">
+          {allproduct? allproduct.map((item)=>
+              <div key={item.id} className='flex flex-col justify-center text-left hover:scale-105 transition delay-75 p-8 '>
+                <Link href={`/products/${ReplaceSpaceWithDash(item.name)}`} >                      
+                  <Image key={item.id} src={urlForImage(item.image).width(200).url()} alt='product' width={300} height={300} className='w-full h-auto ' />
+                </Link>
+                <h1 key={item.id} className='font-bold'>{item.name}</h1>
+                <h2 key={item.id}>{item.producttype}</h2>
+                <h2 key={item.id} className='font-bold text-lg'>${item.price}</h2>
+              </div>
+          
+          ):<div className="flex justify-center align-middle text-2xl font-extrabold">Loading...</div>
+        }
         </Slider>
+        </div>
       );
 }
 
--- src/view/home/productsection.tsx
+++ src/view/home/productsection.tsx
@@ -5,7 +5,7 @@ export default function ProductSection() {
     <section className='flex flex-col justify-center space-y-4 text-center w-full '>
       <h3 className='text-blue-600 font-bold '>PRODUCTS</h3>
       <h2 className=' font-extrabold lg:text-5xl text-4xl '>Check What We Have</h2>
-      <div className='pt-8 pb-48 bg-gray-500 w-full '>
+      <div className='pt-8  w-full '>
           <ProductSlide/>
       </div>
     </section>
