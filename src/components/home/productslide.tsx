bcb65a4ff96bc702fe19d5943c290a878f84cad0
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-07-25 12:48:15 +0500

    slide responsive incomplete yets

--- src/components/home/productslide.tsx
+++ src/components/home/productslide.tsx
@@ -3,7 +3,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { useEffect, useState } from "react";
+import React, { CSSProperties, useEffect, useState } from "react";
 import { client } from '@/lib/sanityClient'
 import Image from 'next/image';
 import { IProduct } from '@/types/types';
@@ -21,6 +21,15 @@ const getProductDataFromSanity = async (): Promise<IProduct[]> => {
   }
 
 };
+
+function HideArrow(props: {  style: any; }) {
+  const { style } = props;
+  return (
+    <div
+      style={{ ...style}}
+    />
+  );
+}
 
 export default  function SimpleSlider():JSX.Element {
   // using state to convert a promise to a valid jsx element
@@ -40,47 +49,63 @@ export default  function SimpleSlider():JSX.Element {
   const settings = {
         dots: false,
         infinite: false,
+        // focusOnSelect: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        nextArrow:<HideArrow  style={"display:none"} />,
+        prevArrow:<HideArrow  style={"display:none"} />,
         responsive: [
           {
             breakpoint: 1024,
             settings: {
               slidesToShow: 3,
+              dots: false,
+              infinite: false,
+              speed: 500,
+              slidesToScroll: 1,
             }
           },
           {
-            breakpoint: 600,
+            breakpoint: 768,
             settings: {
               slidesToShow: 2, 
+              dots: false,
+              infinite: false,
+              speed: 500,
+              slidesToScroll: 1,
             }
           },
           {
-            breakpoint: 480,
+            breakpoint: 300,
             settings: {
               slidesToShow: 1,
+              dots: false,
+              infinite: false,
+              speed: 500,
+              slidesToScroll: 1,
             }
           }
         ]
       };
       
-      return (<div className="mx-8">
-        <Slider {...settings} className="flex ">
-          {allproduct? allproduct.map((item)=>
-              <div key={item.id} className='flex flex-col justify-center text-left hover:scale-105 transition delay-75 p-8 '>
+      return (
+      <>
+        <Slider {...settings} >
+          {allproduct? allproduct.map((item,index)=>
+              <div key={index} className='flex flex-col  text-left hover:scale-105 transition delay-75 bg-gray-50 md:p-8'>
+              {/* // <div key={index} className='flex flex-col  text-left  bg-gray-100 md:pr-8'> */}
                 <Link href={`/products/${ReplaceSpaceWithDash(item.name)}`} >                      
-                  <Image key={item.id} src={urlForImage(item.image).width(200).url()} alt='product' width={300} height={300} className='w-full h-auto ' />
+                  <Image key={index} src={urlForImage(item.image).width(200).url()} alt='product' width={100} height={100} className='w-[300px] lg:w-full h-auto  ' />
                 </Link>
-                <h1 key={item.id} className='font-bold'>{item.name}</h1>
-                <h2 key={item.id}>{item.producttype}</h2>
-                <h2 key={item.id} className='font-bold text-lg'>${item.price}</h2>
-              </div>
-          
-          ):<div className="flex justify-center align-middle text-2xl font-extrabold">Loading...</div>
-        }
+                <h1 key={index} className='font-bold '>{item.name}</h1>
+                <h2 key={index}>{item.producttype}</h2>
+                <h2 key={index} className='font-bold text-lg'>${item.price}</h2>
+              </div>  
+            ):<div className="flex justify-center align-middle text-2xl font-extrabold">Loading...</div>
+          }
         </Slider>
-        </div>
+      </>
       );
 }
 
