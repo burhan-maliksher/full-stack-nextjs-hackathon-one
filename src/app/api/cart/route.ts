6d7246e87785aa293e38fc86cbe747e32934c4e5
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-08-13 12:20:35 +0500

    added product to db successfully

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,4 +1,6 @@
+// "use client";
 import { db, cartTable } from "@/lib/drizzle";
+import { log } from "console";
 import { cookies } from "next/headers";
 import { NextResponse, NextRequest } from "next/server";
 import { v4 as uuid } from "uuid";
@@ -19,34 +21,31 @@ export const POST = async (request: NextRequest) => {
   // console.log(req.product_id);
 
   // generating random user id
-
-  const uid = uuid();
+  var uid = uuid();
   // console.log(uid);
 
   // storing user id in cookies
   const setCookie = cookies();
 
-  const user_id = cookies().get("user_id");
+  const user_id = cookies().get("user_id")?.value;
   // console.log(user_id);
 
   if (!user_id) {
     setCookie.set("user_id", uid);
-    // console.log(user_id);
+    console.log(cookies().get("user_id")?.value);
   }
 
   try {
-    const res = db
+    const res = await db
       .insert(cartTable)
       .values({
-        user_id: cookies().get("user_id")?.value as string,
         product_id: req.product_id,
         quantity: req.quantity,
-        // quantity: 1,
-        // user_id: "test",
-        // product_id: "testid",
-        // quantity: 1,
+        user_id: user_id as string,
       })
       .returning();
+    console.log(res);
+
     return NextResponse.json({ res });
   } catch (error) {
     console.log(error);
