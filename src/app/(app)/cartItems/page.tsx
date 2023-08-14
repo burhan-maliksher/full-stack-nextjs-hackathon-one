db645d8ffa9198e1bdf0bca13539a1e897e64fba
Muhammad Burhan <66227807+burhan-maliksher@users.noreply.github.com>
2023-08-14 18:22:43 +0500

    cart empty ui done

--- src/app/(app)/cartItems/page.tsx
+++ src/app/(app)/cartItems/page.tsx
@@ -8,6 +8,9 @@ import Image from 'next/image';
 import { urlForImage } from '../../../../sanity/lib/image';
 import Link from 'next/link';
 import {RiDeleteBin5Line} from "react-icons/ri"
+import {BiShoppingBag} from "react-icons/bi"
+
+
 // fetching product data from sanity
 const getProductDataFromSanity = async (productList:string[]):Promise<IProductCart[] > => {
   try {
@@ -70,15 +73,19 @@ export default async function CartItems() {
                     <Image src={urlForImage(item.image).width(100).url()} alt='product' width={100} height={100} className='w-64   h-auto border rounded-xl' />
 
                     <div className="flex flex-col justify-center md:justify-start md:space-y-4 mt-4">
-                      <div className='flex'>
-                        <h1 key={item._id} className='font-light text-2xl justify-start'>{item.name}</h1>
-                        <RiDeleteBin5Line className='justify-end w-10'/>
+                      <div className='flex w-[16rem] md:w-[20rem] lg:w-[24rem] xl:w-[30rem]  '>
+                        <div className='w-[14rem] md:w-[18rem] lg:w-[22rem] xl:w-[28rem]'>
+                          <h1 key={item._id} className='font-light text-2xl '>{item.name}</h1>
+                        </div>
+                        <button className='w-[2rem]'>
+                          <RiDeleteBin5Line className=' w-6 h-6'/>
+                        </button>
                       </div>
                       <h2 key={item._id} className="text-xl font-bold text-gray-400">{item.producttype}</h2>
                       <h2 key={item._id} className="text-xl font-medium text-gray-700">Delivery Estimation</h2>
                       <h2 key={item._id} className="text-xl font-semibold text-yellow-400">5 Working Days</h2>
-                      <div className='flex'>
-                        <h2 key={item._id} className="text-xl font-bold text-gray-700 justify-start">${item.price}</h2>
+                      <div className='flex w-[16rem] md:w-[20rem] lg:w-[24rem] xl:w-[30rem]'>
+                        <h2 key={item._id} className="text-xl font-bold text-gray-700 w-[8rem] md:w-[12rem] lg:w-[16rem] xl:w-[20rem]">${item.price}</h2>
                         <div className='justify-end'>- 1 +</div>
                       </div>
                     </div>
@@ -90,27 +97,33 @@ export default async function CartItems() {
             )):<div className=' hidden '></div>    
         }
       </div> 
-
-      {/* order summary */}
-      <div className='text-lg flex flex-col space-y-4 mt-12 lg:mt-0'>
-        <h3 className='text-2xl font-bold'>Order Summary</h3>
-        <div className='flex flex-row space-x-2 '>
-          <div className='justify-start'>Quantity</div>
+        {data.length!==0 &&
+      // {/* order summary */}
+      <div className='text-lg flex flex-col space-y-4 mt-12 lg:mt-0 lg:ml-8'>
+        
+        <h3 className='text-2xl font-bold text-center md:text-left'>Order Summary</h3>
+        <div className='flex flex-row space-x-2 w-[18rem] md:w-[45rem] lg:w-[24rem] xl:w-[30rem] '>
+          <div className='w-[10rem] md:w-[32rem] lg:w-[16rem] xl:w-[20rem]'>Quantity</div>
           <div className='justify-end'>0 Products</div>
         </div>
-        <div className='flex space-x-2  '>
-          <div className=' justify-start'>Sub Total</div>
+        
+        <div className='flex space-x-2 w-[18rem] md:w-[45rem] lg:w-[24rem] xl:w-[30rem] '>
+          <div className=' w-[10rem] md:w-[32rem] lg:w-[16rem] xl:w-[20rem]'>Sub Total</div>
           <div className=' justify-end'>$0</div>
         </div>
         <Link href={"/allProducts"} className="bg-slate-800 w-full h-auto text-xl font-bold p-4 text-white flex justify-center space-x-4">
             Process to Checkout
         </Link>
       </div>
+      }
     </div>
     </div>
     {
-      data.length===0?<div className='flex w-full  h-full m-36 pb-24 align-middle justify-center '>
-        <h1 className='text-3xl md:text-4xl lg:text-5xl'>No Item Found</h1>
+      data.length===0?<div className='flex w-full  h-full pb-24   justify-center '>
+        <div className='text-3xl md:text-4xl lg:text-5xl flex flex-col text-center items-center'>
+         <BiShoppingBag className='w-14 h-14 md:w-20 md:h-20 lg:w-56 lg:h-56'/>
+          <h1>Your shopping bag is empty</h1>
+        </div>
       </div>:<div className='hidden'></div>
       
     }   
